37ba171e45083ca664f3e0d2eb63c79d7015399b
ONBASE\rbrightwell <richard.brightwell@hyland.com>
2021-02-06 01:27:39 -0500

    Added IoC Project and configured presentation layer for IoC

---
+++ CleanArch/CleanArch.Infra.IoC/DependancyContainer.cs
@@ -0,0 +1,23 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.IoC
+{
+    public class DependancyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Application Layer
+            services.AddScoped<ICourseService, CourseService>();
+
+            // Data Layer
+            services.AddScoped<ICourseRepository, CourseRepository>();
+        }
+    }
+}
